1573147f2c6fc74422f97b15943a0cfe71f69c6f
JJWasyl <37805855+JJWasyl@users.noreply.github.com>
2020-03-01 18:50:14 -0600

    Column color codes

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,5 @@ fn main() {
     let data: &[&[u32]] = &[&[32, 5, 17], &[20, 10, 15]];
     let labels: &[&str] = &[&"2017", &"some year"];
     let columns: &[&str] = &[&"Boys", &"Girls", &"Neuters"];
-    print_static(data, labels, columns);
+    print_static(data, labels, columns, true, true);
 }
-
--- src/static_plot/mod.rs
+++ src/static_plot/mod.rs
@@ -1,37 +1,102 @@
 use crossterm::{
-    execute,
+    cursor, execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
     ExecutableCommand, Result,
-    cursor,
 };
 use std::io::{stdout, Write};
 
-pub fn print_static(data: &[&[u32]], rows: &[&str], columns: &[&str]) {
+pub fn print_static(
+    data: &[&[u32]],
+    rows: &[&str],
+    columns: &[&str],
+    show_column_labels: bool,
+    show_legend: bool,
+) {
     let mut max_lab = 0;
     let mut max_col = 0;
-    rows.into_iter().for_each(|s| max_lab = if s.len() > max_lab {s.len()} else {max_lab});
-    columns.into_iter().for_each(|s| max_col = if s.len() > max_col {s.len()} else {max_col});
+    // cp = color palette, mocked for now
+    let cp: Vec<Color> = vec![Color::Red, Color::Cyan, Color::White, Color::Green];
+    let mut cp_iter = cp.iter();
+    rows.into_iter()
+        .for_each(|s| max_lab = if s.len() > max_lab { s.len() } else { max_lab });
+    columns
+        .into_iter()
+        .for_each(|s| max_col = if s.len() > max_col { s.len() } else { max_col });
+    if show_legend {
+        let mut offset = max_lab + 2;
+        if show_column_labels {
+            offset += max_col + 2;
+        }
+        execute!(stdout(), Print(" ".repeat(offset)),).unwrap();
+        for col_label in columns {
+            let color = match cp_iter.next() {
+                Some(col) => col,
+                None => {
+                    cp_iter = cp.iter();
+                    cp_iter.next().unwrap()
+                }
+            };
+            execute!(
+                stdout(),
+                Print(String::from(*col_label) + &" "),
+                SetForegroundColor(*color),
+                Print(generate_bar(1) + &" ".repeat(4)),
+                ResetColor
+            )
+            .unwrap()
+        }
+        execute!(stdout(), Print("\n"),).unwrap();
+    }
     for (row_idx, row) in data.iter().enumerate() {
-        print_label_group(rows[row_idx], max_lab, row, columns, max_col);
+        print_label_group(
+            rows[row_idx],
+            max_lab,
+            row,
+            columns,
+            max_col,
+            show_column_labels,
+        );
     }
 }
 
-fn print_label_group(label: &str, max_label_len: usize, columns: &[u32], column_labels: &[&str], max_col_len: usize) {
-    print_label(&(label.to_owned() + &(" ".repeat(max_label_len - label.len()))), Color::White);
+fn print_label_group(
+    label: &str,
+    max_label_len: usize,
+    columns: &[u32],
+    column_labels: &[&str],
+    max_col_len: usize,
+    show_column_labels: bool,
+) {
+    // cp = color palette, mocked for now
+    let cp: Vec<Color> = vec![Color::Red, Color::Cyan, Color::White, Color::Green];
+    print_label(&norm_label(label, max_label_len), Color::White);
+    let mut cp_iter = cp.iter();
     for i in 0..columns.len() {
+        execute!(stdout(), cursor::SavePosition,).unwrap();
+        if show_column_labels {
+            print_label(&norm_label(column_labels[i], max_col_len), Color::White);
+        }
+        let color = match cp_iter.next() {
+            Some(col) => col,
+            None => {
+                cp_iter = cp.iter();
+                cp_iter.next().unwrap()
+            }
+        };
+        print_bar(columns[i], *color);
         execute!(
             stdout(),
-            cursor::SavePosition,
-        ).unwrap();
-        print_label(&(column_labels[i].to_owned() + &(" ".repeat(max_col_len - column_labels[i].len()))), Color::White);
-        print_bar(columns[i], Color::White);
-        execute!(
-            stdout(),
+            Print("\n"),
             cursor::RestorePosition,
             cursor::MoveDown(1),
-        ).unwrap();
-    };
+        )
+        .unwrap();
+    }
     execute!(stdout(), cursor::MoveToNextLine(0)).unwrap();
+}
+
+fn norm_label(label: &str, max_len: usize) -> String {
+    label.to_owned() + &(" ".repeat(max_len - label.len()))
 }
 
 fn print_label(label: &str, color: Color) {
@@ -41,7 +106,8 @@ fn print_label(label: &str, color: Color) {
         Print(label),
         Print(": "),
         ResetColor,
-    ).unwrap();
+    )
+    .unwrap();
 }
 
 fn print_bar(size: u32, color: Color) {
@@ -49,15 +115,16 @@ fn print_bar(size: u32, color: Color) {
         stdout(),
         SetForegroundColor(color),
         Print(generate_bar(size)),
-        Print(format!(" {}\n", size)),
+        Print(format!(" {}", size)),
         ResetColor
-    ).unwrap();
+    )
+    .unwrap();
 }
 
 fn generate_bar(amount: u32) -> String {
     let mut result = String::from("");
     for _ in 0..amount {
         result.push_str(&"▄");
-    };
+    }
     result
 }
